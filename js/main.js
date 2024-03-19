16bae43c5a9e5df9a6ead64f19f0537c70844cff
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2024-03-19 21:33:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,53 @@
+import {Services} from './api.js';
 // Định danh các phần tử DOM
 const searchInput = document.getElementById("search-input");
-
-let currentDataSource = "tradapan";
 let allDataBase;
-
-async function getDataBaseStore(dataSource) {
-  const storedDatabase = localStorage.getItem(dataSource);
-  if (storedDatabase) {
-    return JSON.parse(storedDatabase);
-  } else {
-    try {
-      const resBook = await fetch(`https://658bef26859b3491d3f51938.mockapi.io/${dataSource}`);
-      let database = await resBook.json();
-      localStorage.setItem(dataSource, JSON.stringify(database));
-      return database;
-    } catch (error) {
-      console.error(`Error parsing ${dataSource} list from local storage:`, error);
-      return [];
-    }
-  }
-}
-
-function displayDataBase(DataBaseList) {
-  const tbody = document.querySelector("#table-book tbody");
-  tbody.innerHTML = '';
-  DataBaseList.forEach((DataBase, index) => {
-    const newRow = createTableRow(DataBase.question, DataBase.answer);
-    tbody.appendChild(newRow);
-  });
+const service = new Services();
+function renderList(DataBaseList) {
+    const tbody = document.querySelector("#table-book tbody");
+    tbody.innerHTML = '';
+    DataBaseList.forEach((DataBase, index) => {
+        const newRow = createTableRow(DataBase.question, DataBase.answer);
+        tbody.appendChild(newRow);
+    });
 }
 function createTableRow(question, answer) {
-  const newRow = document.createElement('tr');
-  newRow.classList.add('database-item');
-  newRow.innerHTML = `
+    const newRow = document.createElement('tr');
+    newRow.classList.add('database-item');
+    newRow.innerHTML = `
         <td>${question}</td>
         <td>${answer}</td>
     `;
-  return newRow;
+    return newRow;
 }
 
 // Tìm kiếm
 const SearchDataBase = (event) => {
-  const filter = [];
-  const searchValue = event.target.value.toLowerCase();
-  allDataBase.forEach((database) => {
-    if (database.search.toLowerCase().includes(searchValue)) {
-      filter.push(database);
+    const filter = [];
+    const searchValue = event.target.value.toLowerCase();
+    if (searchValue) {
+        allDataBase.forEach((database) => {
+            if (database.search.toLowerCase().includes(searchValue)) {
+                filter.push(database);
+            }
+        });
+        renderList(filter);
+    } else {
+        renderList(allDataBase);
     }
-  });
-  if (searchValue) {
-    displayDataBase(filter);
-  } else {
-    displayDataBase(allDataBase);
-  }
 };
-
+async function initializePage() {
+    allDataBase = await service.getDatas();
+    renderList(allDataBase);
+}
 // Sự kiện tìm kiếm
 searchInput.addEventListener('keyup', SearchDataBase);
 
-async function initializePage() {
-  allDataBase = await getDataBaseStore(currentDataSource);
-  displayDataBase(allDataBase);
-}
-
-document.addEventListener("DOMContentLoaded", initializePage);
+// Sự kiện khi trang web được tải
+window.onload = async () => {
+    try {
+        await initializePage();
+    } catch (error) {
+        console.error(error);
+    }
+};
